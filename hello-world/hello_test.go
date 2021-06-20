6263f93893ae8333190bc5549a8b90d5d94678f1
Aron Naylor <aron-naylor@outlook.com>
2021-06-20 15:16:40 +0100

    refactor code and tests

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Aron")
-	want := "Hello, Aron"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Name provided", func(t *testing.T) {
+		got := Hello("Aron")
+		want := "Hello, Aron"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("No name provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
