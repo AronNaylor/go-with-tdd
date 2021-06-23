2d06d89423edf110eec8d4772c417411e893ea7e
Aron Naylor <aron-naylor@outlook.com>
2021-06-23 11:12:29 +0100

    arrs-slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -9,3 +9,23 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var totals []int
+
+	for _, numbers := range numbersToSum {
+		totals = append(totals, Sum(numbers))
+	}
+
+	return totals
+}
+
+func SumAllTails(tailsToSum ...[]int) (tails []int) {
+
+	for _, numbers := range tailsToSum {
+		sliceTail := numbers[1:]
+		tails = append(tails, Sum(sliceTail))
+	}
+
+	return tails
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arrs
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -14,5 +17,26 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{3, 4})
+	want := []int{3, 7}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("return a slice with the tails of each slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
